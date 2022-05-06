d4458ab75bc2f8762282395bff790ef5b350c9b5
Yuliya Kryvarot <yuliya_kryvarot@epam.com>
2022-05-06 09:28:18 +0200

    refactor: use pipeline

--- task2.js
+++ task2.js
@@ -1,16 +1,46 @@
 import fs from 'fs';
 import path from 'path';
 import csv from 'csvtojson';
+import { pipeline, Transform } from 'stream';
 
-const writeCsvToTxtLineByLine = (fromPath, toPath) => {
+const writeCsvToTxtLineByLine = async (fromPath, toPath) => {
     try {
-        const writeStream = fs.createWriteStream(toPath);
-        csv()
-            .fromFile(fromPath)
-            .subscribe()
-            .pipe(writeStream);
+        const transform = new Transform({
+            transform: function(chunk, encoding, callback) {
+                callback(null, converter(JSON.parse(chunk.toString())));
+            }
+        })
+
+        const converter = (json) =>  {
+            const keys = Object.keys(json)[0].split(';');
+            const values = Object.values(json)[0].split(';');
+            const result = {};
+
+            for (let i = 0; i < keys.length; i++) {
+                if (values) {
+                    result[keys[i]] = values[i];
+                } else {
+                    result[keys[i][0]] = keys[i][1];
+                }
+            }
+
+            return Buffer.from(JSON.stringify(result));
+        };
+
+        await pipeline(
+            csv().fromFile(fromPath),
+            transform,
+            fs.createWriteStream(toPath),
+            err => {
+                if (err) {
+                    console.log('Pipeline encountered an error:', err.message);
+                } else {
+                    console.log('Pipeline ended');
+                }
+            }
+        );
     } catch (err) {
-        console.error(err.message);
+       console.error(err.message);
     }
 }
 
