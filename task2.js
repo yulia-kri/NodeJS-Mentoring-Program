76d2abaca3631da9eb5398cb9e0ec7a6b1f43eab
Yuliya Kryvarot <yuliya_kryvarot@epam.com>
2022-05-23 19:58:09 +0200

    refactor: rework transform function

--- task2.js
+++ task2.js
@@ -1,49 +1,29 @@
 import fs from 'fs';
 import path from 'path';
 import csv from 'csvtojson';
-import { pipeline, Transform } from 'stream';
+import { pipeline } from 'stream';
 
 const writeCsvToTxtLineByLine = async (fromPath, toPath) => {
-    try {
-        const transform = new Transform({
-            transform: function(chunk, encoding, callback) {
-                callback(null, converter(JSON.parse(chunk.toString())));
-            }
-        })
-
-        const converter = (json) =>  {
-            const keys = Object.keys(json)[0].split(';');
-            const values = Object.values(json)[0].split(';');
-            const result = {};
-
-            for (let i = 0; i < keys.length; i++) {
-                if (values) {
-                    result[keys[i]] = values[i];
-                } else {
-                    result[keys[i][0]] = keys[i][1];
+    const keyToRemove = 'Amount';
+    const readStream = fs.createReadStream(fromPath);
+    const writeStream  =  fs.createWriteStream(toPath, 'utf8');
+    const transform = csv({downstreamFormat: 'line'})
+        .subscribe((item) => {
+            Object.entries(item).forEach(([key, value]) => {
+                delete item[key];
+                if(key !== keyToRemove) {
+                    item[key.toLowerCase()] = value;
                 }
-            }
+            })
+    });
 
-            return Buffer.from(JSON.stringify(result));
-        };
-
-        await pipeline(
-            csv().fromFile(fromPath),
-            transform,
-            fs.createWriteStream(toPath),
-            err => {
-                if (err) {
-                    console.log('Pipeline encountered an error:', err.message);
-                } else {
-                    console.log('Pipeline ended');
-                }
-            }
-        );
-    } catch (err) {
-       console.error(err.message);
-    }
+    pipeline(readStream, transform, writeStream, (err) =>{
+        if (err) {
+            console.error('Error:', err.message)
+        }
+    });
 }
 
-const csvFilePath = path.join(__dirname, 'csv', 'Book1.csv');
+const csvFilePath = path.join(__dirname, 'csv', 'book.csv');
 const txtFilePath = path.join(__dirname, 'new_file.txt');
 writeCsvToTxtLineByLine(csvFilePath, txtFilePath);
