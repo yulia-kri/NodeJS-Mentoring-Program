8ec59e0a176e7db79dfed74eebe9eaaef0b29d98
Yuliya Kryvarot <yuliya_kryvarot@epam.com>
2022-07-08 23:56:05 +0200

    feat: add logger

--- src/index.ts
+++ src/index.ts
@@ -3,23 +3,33 @@ import express from 'express';
 import { routes } from './controllers/routes';
 import { associate } from './data-access/associations';
 import { sequelize } from './data-access/models';
+import { logger, errorHandling, traceMethodCalls } from './logger';
 
 const app = express();
 const router = express.Router();
 
-const PORT = 3000;
+const PORT = 3001;
+
+process
+    .on('unhandledRejection', (reason, promise) => {
+        logger.error(`Unhandled Rejection at: ${promise}, reason: ${reason}`);
+    })
+    .on('uncaughtException', (err, origin) => {
+        logger.error(`Caught exception: ${err}, exception origin: ${origin}`);
+    });
 
 routes(router);
 
 app.use(express.json());
-
+app.use(traceMethodCalls);
 app.use(router);
+app.use(errorHandling);
 
 app.listen(PORT, async () => {
-    console.log(`the app is running on ${PORT}`);
+    logger.info(`the app is running on ${PORT}`);
 
     await sequelize.authenticate();
-    console.log('database connected');
+    logger.info('database connected');
 
     associate();
 });
